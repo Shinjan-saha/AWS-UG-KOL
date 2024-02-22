4f9baa023396039a6323be582fcf6a3c136cb9a9
Shinjan-saha <shinjansaha02@gmail.com>
2024-02-22 22:04:47 +0530

    Small Changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -440,34 +440,25 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
-  let slideIndex = 1;
-showSlides(slideIndex);
+  var slideIndex = 0;
+    showSlides();
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+    function showSlides() {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}
+        slides[slideIndex-1].style.display = "block";
+        setTimeout(showSlides, 2); // Change image every 2 seconds
+    }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-
-}
-let slideInterval = setInterval(function() {
-    plusSlides(1); // Change the argument if you want to change the slide interval
-  }, 2000);
-  
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
