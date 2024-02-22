ec55cda31a87175f1593a920376e25733a3f33dd
Shinjan-saha <shinjansaha02@gmail.com>
2024-02-22 22:16:29 +0530

    Small Fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -452,7 +452,7 @@ document.addEventListener('DOMContentLoaded', function () {
         slideIndex++;
         if (slideIndex > slides.length) {slideIndex = 1}
         slides[slideIndex-1].style.display = "block";
-        setTimeout(showSlides, 2000); // Change image every 2 seconds
+        setTimeout(showSlides, 4000); // Change image every 4 seconds
     }
 
     function plusSlides(n) {
